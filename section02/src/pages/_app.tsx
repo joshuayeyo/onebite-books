7f9f50459f6789ea0ad70c3564bd8477299b7e9a
joshuayeyo <chiikawayeyo@gmail.com>
2025-01-28 13:42:17 +0900

    Section2: 2-4

--- section02/src/pages/_app.tsx
+++ section02/src/pages/_app.tsx
@@ -2,18 +2,25 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Link from "next/link";
 import {useRouter} from "next/router";
+import {useEffect} from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
     const router = useRouter();
     const onClickButton = () => {
         router.push("/test")
     };
+
+    // 특정 페이지 pre-fetch
+    useEffect(() => {
+        router.prefetch("/test");
+    }, []);
   return (
     <>
       <header>
           <Link href={"/"}>index</Link>
           &nbsp;
-          <Link href={"/search"}>search</Link>
+          {/* prefetch={false}로 설정하면 해당 페이지는 prefetch 되지 않음 */}
+          <Link href={"/search"} prefetch={false}>search</Link>
           &nbsp;
           <Link href={"/book/1"}>book/1</Link>
           <div>
--- section02/src/pages/search/index.tsx
+++ section02/src/pages/search/index.tsx
@@ -1,9 +1,3 @@
-import { useRouter } from "next/router";
-
 export default function Page() {
-    const router = useRouter();
-
-    const { q } = router.query;
-
     return <h1>Search</h1>;
 }
